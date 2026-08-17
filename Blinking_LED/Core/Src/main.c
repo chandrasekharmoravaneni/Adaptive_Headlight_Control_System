05203fd9d201cf124c132c27bfeedfbb5a30e7a5
chandrasekharmoravaneni <chandrasekhar.moravaneni.naidu@gmail.com>
2026-08-17 14:31:21 +0200

    Added ADC_PWM Controll example

--- Blinking_LED/Core/Src/main.c
+++ Blinking_LED/Core/Src/main.c
@@ -42,11 +42,15 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 UART_HandleTypeDef huart2;
-char msg[100];
-uint8_t rx_data;
 
 /* USER CODE BEGIN PV */
+char msg[100];
+uint8_t rx_data;
+uint32_t adc_value;
+uint32_t voltage;
 
 /* USER CODE END PV */
 
@@ -54,6 +58,7 @@ uint8_t rx_data;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -116,6 +121,17 @@ void UART_GPIO_CONTROLLER(void){
 
 	}
 }
+void ADC_UART_MONITOR(void){
+	if(HAL_ADC_PollForConversion(&hadc1,HAL_MAX_DELAY)==HAL_OK){
+		adc_value = HAL_ADC_GetValue(&hadc1);
+		voltage = (adc_value * 3300)/4095;
+		sprintf(msg, "ADC VALUE: %lu, Voltage : %lu mV \r\n",adc_value,voltage);
+		HAL_UART_Transmit(&huart2,(uint8_t*)msg,strlen(msg),HAL_MAX_DELAY);
+
+	}
+	HAL_ADC_Stop(&hadc1);
+
+}
 /* USER CODE END 0 */
 
 /**
@@ -148,7 +164,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
+  HAL_ADC_Start(&hadc1);
 
   /* USER CODE END 2 */
 
@@ -157,8 +175,9 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+	  ADC_UART_MONITOR();
+	  HAL_Delay(500);
 
-	  UART_GPIO_CONTROLLER();
 
 
     /* USER CODE BEGIN 3 */
@@ -210,6 +229,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
